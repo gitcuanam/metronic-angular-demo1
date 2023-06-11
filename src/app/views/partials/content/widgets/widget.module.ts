34071c82d0ab3186f574cd219ab0d25ea2bdcf12
gitcuanam <nam.tranphuong.evotek@gmail.com>
2023-06-11 12:38:38 +0700

    add strict template type check: ngx-scroll

--- src/app/views/partials/content/widgets/widget.module.ts
+++ src/app/views/partials/content/widgets/widget.module.ts
@@ -1,23 +1,29 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CoreModule } from '../../../../core/core.module';
+import {
+  CUSTOM_ELEMENTS_SCHEMA,
+  NgModule,
+} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { NgScrollbarModule } from 'ngx-scrollbar';
+
+import { CoreModule } from '../../../../core/core.module';
 // Datatable
 import { DataTableComponent } from './general/data-table/data-table.component';
+import { Timeline2Component } from './timeline2/timeline2.component';
 // General widgets
 import { Widget1Component } from './widget1/widget1.component';
-import { Widget4Component } from './widget4/widget4.component';
-import { Widget5Component } from './widget5/widget5.component';
 import { Widget12Component } from './widget12/widget12.component';
 import { Widget14Component } from './widget14/widget14.component';
 import { Widget26Component } from './widget26/widget26.component';
-import { Timeline2Component } from './timeline2/timeline2.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
+import { Widget4Component } from './widget4/widget4.component';
+import { Widget5Component } from './widget5/widget5.component';
 
 @NgModule({
 	declarations: [
@@ -45,6 +51,7 @@ import { MatSortModule } from '@angular/material/sort';
 	imports: [
 		CommonModule,
 		PerfectScrollbarModule,
+		NgScrollbarModule,
 		MatTableModule,
 		CoreModule,
 		MatIconModule,
