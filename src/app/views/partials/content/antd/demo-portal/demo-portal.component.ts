a4f5a70d39982827c848d889294425cfb2b692f5
gitcuanam <nam.tranphuong.evotek@gmail.com>
2023-10-12 23:15:03 +0700

    finish simple portal

--- src/app/views/partials/content/antd/demo-portal/demo-portal.component.ts
+++ src/app/views/partials/content/antd/demo-portal/demo-portal.component.ts
@@ -1,7 +1,15 @@
-import { PortalOutlet } from '@angular/cdk/portal';
 import {
+  Portal,
+  PortalOutlet,
+  TemplatePortal,
+} from '@angular/cdk/portal';
+import {
+  AfterViewInit,
   Component,
   OnInit,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
 } from '@angular/core';
 
 @Component({
@@ -9,11 +17,27 @@ import {
   templateUrl: './demo-portal.component.html',
   styleUrls: ['./demo-portal.component.scss']
 })
-export class DemoPortalComponent implements OnInit {
+export class DemoPortalComponent implements OnInit, AfterViewInit {
 
-  portalOutlet: PortalOutlet;
+  portalOutlet?: PortalOutlet;
+  selectedPortal?: Portal<unknown>;
 
-  constructor() { }
+  noDataTemplatePortal?: TemplatePortal<unknown>;
+  @ViewChild('noData') noDataTemplate?: TemplateRef<unknown>;
+
+  templatePortal1?: TemplatePortal<unknown>;
+  @ViewChild('templateData1') templateData1?: TemplateRef<unknown>;
+
+  constructor(private _viewContainerRef: ViewContainerRef) {
+  }
+
+  ngAfterViewInit(): void {
+    // TODO: Tại sao khi đưa dòng này vào constructor lại báo lỗi run-time Cannot read properties of undefined (reading 'createEmbeddedView')
+    // this.noDataTemplatePortal = new TemplatePortal(this.noDataTemplate, this._viewContainerRef);
+    this.noDataTemplatePortal = this.noDataTemplate ? new TemplatePortal(this.noDataTemplate, this._viewContainerRef) : undefined;
+    this.templatePortal1 = this.templateData1 ? new TemplatePortal(this.templateData1, this._viewContainerRef) : undefined;
+    this.selectedPortal = this.templatePortal1;
+  }
 
   ngOnInit(): void {
   }
